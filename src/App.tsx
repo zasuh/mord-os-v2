d30295731348787ca658900c6e106ee639d1db76
Žane Suhadolnik <zanesuhi@gmail.com>
2022-12-05 10:25:22 +0100

    add typescript, webpack config, linting, prettier

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+export interface HelloWorldProps {}
+function App() {
+	return <h1>Hello World!</h1>;
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './index.js',
+    entry: './index.tsx',
     mode: 'development',
     output: {
         path: path.resolve(__dirname, './dist'),
@@ -19,20 +20,31 @@ module.exports = {
         liveReload: true,
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.json'],
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
+                test: /\.(ts|tsx)$/,
+                loader: "awesome-typescript-loader",
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: "css-loader",
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'public', 'index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./src/index.css",
         }),
     ],
 };
